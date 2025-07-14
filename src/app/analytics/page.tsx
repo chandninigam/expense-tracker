e43348f5a4a1f348476471139f481ece35494758
chandni nigam <nigamchandni139@gmail.com>
2025-07-14 10:43:53 +0530

    Added chart

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,3 +1,57 @@
+"use client";
+
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const COLORS: string[] = [
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#AF19FF",
+];
+
+const expenseData = [
+  { category: "Food", amount: 1200 },
+  { category: "Travel", amount: 800 },
+  { category: "Rent", amount: 3000 },
+  { category: "Shopping", amount: 900 },
+];
+
 export default function Analytics() {
-  return <div>I am Analytics Dashboard</div>;
+  return (
+    <div style={{ width: "100%", height: "300px" }}>
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            data={expenseData}
+            dataKey="amount"
+            nameKey="category"
+            cx="50%"
+            cy="50%"
+            outerRadius={100}
+            fill="#8884d8"
+            label
+          >
+            {expenseData.map((_, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <div>
+            <Tooltip />
+            <Legend />
+          </div>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
