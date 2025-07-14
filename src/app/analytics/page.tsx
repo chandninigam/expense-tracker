69a7679306aebb7cd891ac19a9b763eb3d292c81
chandni nigam <nigamchandni139@gmail.com>
2025-07-14 11:54:27 +0530

    Added chart for monthly and yearly

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import {
-  PieChart,
-  Pie,
-  Cell,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
+import "../../style/analytics.css";
 
 const COLORS: string[] = [
   "#0088FE",
@@ -26,32 +20,107 @@ const expenseData = [
 
 export default function Analytics() {
   return (
-    <div style={{ width: "100%", height: "300px" }}>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            data={expenseData}
-            dataKey="amount"
-            nameKey="category"
-            cx="50%"
-            cy="50%"
-            outerRadius={100}
-            fill="#8884d8"
-            label
-          >
-            {expenseData.map((_, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={COLORS[index % COLORS.length]}
-              />
+    <div className="analytics-wrapper">
+      <div className="chart-wrapper">
+        <h3>Monthly Expense</h3>
+        <div className="chart-detail-wrapper">
+          <ResponsiveContainer width="50%" height={300}>
+            <PieChart>
+              <Pie
+                data={expenseData}
+                dataKey="amount"
+                nameKey="category"
+                cx="50%"
+                cy="50%"
+                outerRadius={100}
+                fill="#8884d8"
+                label
+              >
+                {expenseData.map((_, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+              <div>
+                <Tooltip />
+              </div>
+            </PieChart>
+          </ResponsiveContainer>
+          <div className="chart-categories-wrapper">
+            {expenseData.map((item, index) => (
+              <div key={index} className="detail-wrapper">
+                <span
+                  style={{
+                    width: "16px",
+                    height: "16px",
+                    backgroundColor: COLORS[index % COLORS.length],
+                    display: "inline-block",
+                    borderRadius: "4px",
+                  }}
+                />
+                <span className="">
+                  {item.category}: ₹{item.amount}
+                </span>
+              </div>
             ))}
-          </Pie>
-          <div>
-            <Tooltip />
-            <Legend />
           </div>
-        </PieChart>
-      </ResponsiveContainer>
+        </div>
+      </div>
+      <div className="chart-wrapper">
+        <h3>Yearly Expense</h3>
+        {false ? (
+          <div className="chart-detail-wrapper">
+            <ResponsiveContainer width="50%" height={300}>
+              <PieChart>
+                <Pie
+                  data={expenseData}
+                  dataKey="amount"
+                  nameKey="category"
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={100}
+                  fill="#8884d8"
+                  label
+                >
+                  {expenseData.map((_, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                </Pie>
+                <div>
+                  <Tooltip />
+                </div>
+              </PieChart>
+            </ResponsiveContainer>
+            <div className="chart-categories-wrapper">
+              {expenseData.map((item, index) => (
+                <div key={index} className="detail-wrapper">
+                  <span
+                    style={{
+                      width: "16px",
+                      height: "16px",
+                      backgroundColor: COLORS[index % COLORS.length],
+                      display: "inline-block",
+                      borderRadius: "4px",
+                    }}
+                  />
+                  <span className="">
+                    {item.category}: ₹{item.amount}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="fallback">
+            <h4>Data must cover more than one month.</h4>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
